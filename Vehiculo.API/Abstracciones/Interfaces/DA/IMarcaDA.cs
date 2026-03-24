817d750fa4cb91ef55d575d2063e7f5bd0f6fb17
Micheller24 <mrodriguez90225@ufide.ac.cr>
2026-03-23 19:11:42 -0600

    Agregando pp03

---
+++ Vehiculo.API/Abstracciones/Interfaces/DA/IMarcaDA.cs
@@ -0,0 +1,10 @@
+﻿using Abstracciones.Modelos;
+using System.Text.RegularExpressions;
+
+namespace Abstracciones.Interfaces.DA
+{
+    public interface IMarcaDA
+    {
+        Task<IEnumerable<Marca>> Obtener();
+    }
+}
---
+++ Vehiculo.API/BD/dbo/Stored Procedures/ObtenerVehiculos.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[ObtenerVehiculos]
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+
+SELECT Vehiculo.id, Vehiculo.idModelo, Vehiculo.Color, Vehiculo.Placa, Vehiculo.Año, Vehiculo.Precio, Vehiculo.CorreoPropietario, Vehiculo.TelefonoPropietario, Modelos.Nombre, Marcas.Nombre AS Expr1
+FROM     Vehiculo INNER JOIN
+                  Modelos ON Vehiculo.idModelo = Modelos.id INNER JOIN
+				  Marcas ON Modelos.idMarca = Marcas.id
+
+
+END
